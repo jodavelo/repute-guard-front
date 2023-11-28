b69836493061558b60574126516db211cf0bc743
José Daniel Vergara Lozano <josedani0630@hotmail.com>
2023-11-28 13:46:12 -0500

    table of malicious ips ordering by datetime

--- components/page/AttacksTable.tsx
+++ components/page/AttacksTable.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState, ChangeEvent, useContext } from 'react';
 import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, TablePagination } from '@material-ui/core';
 import { LayoutContext } from '@/context/layout';
+import { TableSortLabel } from '@mui/material';
 
 interface AttackData {
     ip_address: string;
@@ -9,6 +10,7 @@ interface AttackData {
     country_name: string;
     ISP: string;
     host: string;
+    detected_at: string;
 }
 
 const lightThemeStyles = {
@@ -30,14 +32,6 @@ const AttacksTable = () => {
 
     const themeStyles = isDarkTheme ? darkThemeStyles : lightThemeStyles;
 
-    useEffect(() => {
-        fetch(`http://localhost:8000/attacks/?page=${page + 1}&page_size=${rowsPerPage}`)
-            .then(response => response.json())
-            .then(data => setData(data));
-    }, [page, rowsPerPage]);
-
-
-
     const handleChangePage = (event: unknown, newPage: number) => {
         setPage(newPage);
     };
@@ -49,30 +43,79 @@ const AttacksTable = () => {
         setPage(0);
     };
 
+    const handleRequestSort = (property: string) => {
+        const isAsc = orderBy === property && order === 'asc';
+        setOrder(isAsc ? 'desc' : 'asc');
+        setOrderBy(property);
+        // Aquí necesitarás implementar la lógica para ordenar tus datos
+    };
+
+
+    const [order, setOrder] = useState<'asc' | 'desc'>('desc');
+    const [orderBy, setOrderBy] = useState('detected_at');
+    function getComparator(order: any, orderBy: any) {
+        return order === 'desc'
+            ? (a: any, b: any) => descendingComparator(a, b, orderBy)
+            : (a: any, b: any) => -descendingComparator(a, b, orderBy);
+    }
+
+    function descendingComparator(a: any, b: any, orderBy: any) {
+        if (b[orderBy] < a[orderBy]) {
+            return -1;
+        }
+        if (b[orderBy] > a[orderBy]) {
+            return 1;
+        }
+        return 0;
+    }
+
+    useEffect(() => {
+        fetch(`http://localhost:8000/attacks/?page=${page + 1}&page_size=${rowsPerPage}`)
+          .then(response => response.json())
+          .then(data => {
+            const comparator = getComparator(order, orderBy);
+            const sortedData = data.sort(comparator);
+            setData(sortedData);
+          });
+      }, [page, rowsPerPage, order, orderBy]);
+      
+
+
     return (
         <Paper style={themeStyles}>
             <TableContainer>
                 <Table>
                     <TableHead>
                         <TableRow>
-                            <TableCell style={ isDarkTheme ?  { color: 'white' } : { color: 'black' }}>IP Address</TableCell>
-                            <TableCell style={ isDarkTheme ?  { color: 'white' } : { color: 'black' }}>Description</TableCell>
-                            <TableCell style={ isDarkTheme ?  { color: 'white' } : { color: 'black' }}>Fraud Score</TableCell>
-                            <TableCell style={ isDarkTheme ?  { color: 'white' } : { color: 'black' }}>Country</TableCell>
-                            <TableCell style={ isDarkTheme ?  { color: 'white' } : { color: 'black' }}>ISP</TableCell>
-                            <TableCell style={ isDarkTheme ?  { color: 'white' } : { color: 'black' }}>Host</TableCell>
-                            <TableCell style={ isDarkTheme ?  { color: 'white' } : { color: 'black' }}>Status</TableCell>
+                            <TableCell style={isDarkTheme ? { color: 'white' } : { color: 'black' }}>IP Address</TableCell>
+                            <TableCell style={isDarkTheme ? { color: 'white' } : { color: 'black' }}>Description</TableCell>
+                            <TableCell style={isDarkTheme ? { color: 'white' } : { color: 'black' }}>Fraud Score</TableCell>
+                            <TableCell style={isDarkTheme ? { color: 'white' } : { color: 'black' }}>Country</TableCell>
+                            <TableCell style={isDarkTheme ? { color: 'white' } : { color: 'black' }}>ISP</TableCell>
+                            <TableCell style={isDarkTheme ? { color: 'white' } : { color: 'black' }}>Host</TableCell>
+                            <TableCell sortDirection={orderBy === 'detected_at' ? order : false} style={isDarkTheme ? { color: 'white' } : { color: 'black' }}>
+                                <TableSortLabel
+                                    style={isDarkTheme ? { color: 'white' } : { color: 'black' }}
+                                    active={orderBy === 'detected_at'}
+                                    direction={orderBy === 'detected_at' ? order : 'asc'}
+                                    onClick={() => handleRequestSort('detected_at')}
+                                >
+                                    Detected At
+                                </TableSortLabel>
+                            </TableCell>
+                            <TableCell style={isDarkTheme ? { color: 'white' } : { color: 'black' }}>Status</TableCell>
                         </TableRow>
                     </TableHead>
                     <TableBody>
                         {data.map((row) => (
                             <TableRow key={row.ip_address}>
-                                <TableCell style={ isDarkTheme ?  { color: 'white' } : { color: 'black' }}>{row.ip_address}</TableCell>
-                                <TableCell style={ isDarkTheme ?  { color: 'white' } : { color: 'black' }}>{row.description}</TableCell>
-                                <TableCell style={ isDarkTheme ?  { color: 'white' } : { color: 'black' }}>{row.fraud_score}</TableCell>
-                                <TableCell style={ isDarkTheme ?  { color: 'white' } : { color: 'black' }}>{row.country_name}</TableCell>
-                                <TableCell style={ isDarkTheme ?  { color: 'white' } : { color: 'black' }}>{row.ISP}</TableCell>
-                                <TableCell style={ isDarkTheme ?  { color: 'white' } : { color: 'black' }}>{row.host}</TableCell>
+                                <TableCell style={isDarkTheme ? { color: 'white' } : { color: 'black' }}>{row.ip_address}</TableCell>
+                                <TableCell style={isDarkTheme ? { color: 'white' } : { color: 'black' }}>{row.description}</TableCell>
+                                <TableCell style={isDarkTheme ? { color: 'white' } : { color: 'black' }}>{row.fraud_score}</TableCell>
+                                <TableCell style={isDarkTheme ? { color: 'white' } : { color: 'black' }}>{row.country_name}</TableCell>
+                                <TableCell style={isDarkTheme ? { color: 'white' } : { color: 'black' }}>{row.ISP}</TableCell>
+                                <TableCell style={isDarkTheme ? { color: 'white' } : { color: 'black' }}>{row.host}</TableCell>
+                                <TableCell style={isDarkTheme ? { color: 'white' } : { color: 'black' }}>{row.detected_at}</TableCell>
                                 <TableCell style={{ color: 'red', fontWeight: 'bold' }}>Blocked</TableCell>
                             </TableRow>
                         ))}
@@ -80,7 +123,7 @@ const AttacksTable = () => {
                 </Table>
             </TableContainer>
             <TablePagination
-                style={ isDarkTheme ?  { color: 'white' } : { color: 'black' }}
+                style={isDarkTheme ? { color: 'white' } : { color: 'black' }}
                 component="div"
                 count={-1}
                 rowsPerPage={rowsPerPage}
